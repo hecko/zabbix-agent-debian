9350a294a3ca02d5d94e087ccdb82f48f2b49318
root <root@dev-96C40BCD.(none)>
2013-02-15 18:26:39 +0000

    notes

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 ZABBIX_VERSION='2.0.5'
 
 sudo apt-get install dh-make dpkg-dev debhelper devscripts fakeroot git
-git clone https://github.com/hecko/zabbix-agent-debian.git
 wget http://downloads.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/${ZABBIX_VERSION}/zabbix-${ZABBIX_VERSION}.tar.gz
 tar xzfv zabbix-${ZABBIX_VERSION}.tar.gz
 cd zabbix-${ZABBIX_VERSION}
+cp ../meta/zabbix_agentd.conf ./conf
+sync
 dh_make -n -s -p zabbix-agent -e maco@blava.net
 rm debian/*.ex debian/*.EX
 rm debian/README debian/README.Debian
